6217fcc14422aa126c9068a1bcb4288b0cb0961e
gyunge <eksrud0099@cu.ac.kr>
2022-06-29 14:00:07 +0900

    login

--- DU/src/main/java/du/main/controller/MainController.java
+++ DU/src/main/java/du/main/controller/MainController.java
@@ -1,19 +1,22 @@
 package du.main.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
-
-	@RequestMapping(value="/main.do", method = RequestMethod.POST)
-	public String mainPage() {
-		return "main.html";
-	}
 	
-	@RequestMapping(value="/login.do", method = RequestMethod.GET)
-	public String loginPage() {
-		return "login.html";
+	private Logger logger = LoggerFactory.getLogger(MainController.class);
+
+	@RequestMapping(value="/main.do", method = RequestMethod.GET)
+	public String mainPageByGet(HttpServletRequest request) {
+		
+		logger.info(request.getQueryString());
+		
+		return "main.html";
 	}
 }
